f638f1f2315525964b85bcaeca895ab6d90a565d
Stan Dyck <stan.dyck@gmail.com>
2012-03-08 14:20:27 -0800

    First import

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject rundata "1.0.0-SNAPSHOT"
+  :description "A simple library to convert data log files into csv files."
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.4.0"]
+                     [lein-marginalia "0.7.0"]])
+
+
---
+++ test/rundata/test/core.clj
@@ -0,0 +1,6 @@
+(ns rundata.test.core
+  (:use [rundata.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
